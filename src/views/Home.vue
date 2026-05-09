2077bfcc17f048a955191a779f4c9e2cca2a18d4
Keshia Rose <keshia.com@gmail.com>
2026-05-09 06:44:14 -0700

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -721,7 +721,7 @@ if (importName && importIdsRaw) {
           <div class="px-6 pb-5 pt-2">
             <p class="text-xs text-center mb-3" :class="isDark ? 'text-white/40' : 'text-stone-400'">Data last synced on
               May
-              7, 2026</p>
+              9, 2026 6:45 AM PT</p>
             <button @click="closeModal"
               class="w-full bg-coral text-white font-bold py-2.5 rounded-xl cursor-pointer hover:bg-coral/90 transition-colors">
               Got it!
